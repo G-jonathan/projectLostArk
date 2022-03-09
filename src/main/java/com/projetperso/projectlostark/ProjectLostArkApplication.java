fe6e8b9d0cbb858ca15b6e4886e286332dc469b5
jonathan <jonathan.developpeur@gmail.com>
2022-03-09 20:37:33 +0100

    first commit

---
+++ src/main/java/com/projetperso/projectlostark/ProjectLostArkApplication.java
@@ -0,0 +1,13 @@
+package com.projetperso.projectlostark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectLostArkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectLostArkApplication.class, args);
+    }
+
+}
