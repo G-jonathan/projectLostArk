09d1ad66dfe608936373ff6b2d242df92ae1a997
jonathan <jonathan.developpeur@gmail.com>
2022-03-10 13:46:10 +0100

    "wip"

--- src/main/java/com/projetperso/projectlostark/models/SubClass.java
+++ src/main/java/com/projetperso/projectlostark/models/SubClass.java
@@ -4,6 +4,10 @@ import javax.persistence.*;
 
 @Entity
 public class SubClass {
+
+    public enum Engravings {
+        AMELIORATION_D_ETHER
+    }
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
