450653baf70142e8e85e55f9a513a3249a65a9fb
jonathan <jonathan.developpeur@gmail.com>
2022-03-12 10:00:09 +0100

    "wip"

--- src/main/java/com/projetperso/projectlostark/repositories/SubClassRepository.java
+++ src/main/java/com/projetperso/projectlostark/repositories/SubClassRepository.java
@@ -3,7 +3,38 @@ package com.projetperso.projectlostark.repositories;
 import com.projetperso.projectlostark.models.SubClass;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface SubClassRepository extends JpaRepository<SubClass, Integer> {
+
+    public List<SubClass> findByEngraving1(String engraving1);
+
+    public List<SubClass> findByEngraving2(String engraving2);
+
+    public List<SubClass> findByCharacteristic1(String characteristic1);
+
+    public List<SubClass> findByCharacteristic2(String characteristic2);
+
+    public List<SubClass> findByEngraving1AndEngraving2(String engraving1, String engraving2);
+
+    public List<SubClass> findByEngraving1AndCharacteristic1(String engraving1, String characteristic1);
+
+    public List<SubClass> findByEngraving1AndCharacteristic2(String engraving1, String characteristic2);
+
+    public List<SubClass> findByEngraving2AndCharacteristic1(String engraving2, String characteristic1);
+
+    public List<SubClass> findByEngraving2AndCharacteristic2(String engraving2, String characteristic2);
+
+    public List<SubClass> findByCharacteristic1AndCharacteristic2(String characteristic1, String characteristic2);
+
+    public List<SubClass> findByEngraving1AndEngraving2AndCharacteristic1(String engraving1, String engraving2, String characteristic2);
+
+    public List<SubClass> findByEngraving2AndCharacteristic1AndCharacteristic2(String engraving2, String characteristic1, String characteristic2);
+
+    public List<SubClass> findByEngraving1AndCharacteristic1AndCharacteristic2(String engraving1, String characteristic1, String characteristic2);
+
+    public List<SubClass> findByEngraving1AndEngraving2AndCharacteristic2(String engraving1, String engraving2, String characteristic2);
+
+    public List<SubClass> findByEngraving1AndEngraving2AndCharacteristic1AndCharacteristic2(String engraving1, String engraving2, String characteristic1, String characteristic2);
 }
--- src/main/java/com/projetperso/projectlostark/services/impl/SubClassServiceImpl.java
+++ src/main/java/com/projetperso/projectlostark/services/impl/SubClassServiceImpl.java
@@ -5,7 +5,6 @@ import com.projetperso.projectlostark.repositories.SubClassRepository;
 import com.projetperso.projectlostark.services.ISubClassService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -27,88 +26,78 @@ public class SubClassServiceImpl implements ISubClassService {
                 & subClass.getEngraving2().equals(EMPTY)
                 & subClass.getCharacteristic1().equals(EMPTY)
                 & subClass.getCharacteristic2().equals(EMPTY)) {
-        }
-        else if (subClass.getEngraving1().equals(EMPTY)
+            return subClassRepository.findByEngraving1(subClass.getEngraving1());
+        } else if (subClass.getEngraving1().equals(EMPTY)
                 & !subClass.getEngraving2().equals(EMPTY)
                 & subClass.getCharacteristic1().equals(EMPTY)
                 & subClass.getCharacteristic2().equals(EMPTY)) {
-        }
-        else if (subClass.getEngraving1().equals(EMPTY)
+            return subClassRepository.findByEngraving2(subClass.getEngraving2());
+        } else if (subClass.getEngraving1().equals(EMPTY)
                 & subClass.getEngraving2().equals(EMPTY)
                 & !subClass.getCharacteristic1().equals(EMPTY)
                 & subClass.getCharacteristic2().equals(EMPTY)) {
-        }
-        else if (subClass.getEngraving1().equals(EMPTY)
+            return subClassRepository.findByCharacteristic1(subClass.getCharacteristic1());
+        } else if (subClass.getEngraving1().equals(EMPTY)
                 & subClass.getEngraving2().equals(EMPTY)
                 & subClass.getCharacteristic1().equals(EMPTY)
                 & !subClass.getCharacteristic2().equals(EMPTY)) {
-        }
-        else if (!subClass.getEngraving1().equals(EMPTY)
+            return subClassRepository.findByCharacteristic2(subClass.getCharacteristic2());
+        } else if (!subClass.getEngraving1().equals(EMPTY)
                 & !subClass.getEngraving2().equals(EMPTY)
                 & subClass.getCharacteristic1().equals(EMPTY)
                 & subClass.getCharacteristic2().equals(EMPTY)) {
-        }
-        else if (!subClass.getEngraving1().equals(EMPTY)
+            return subClassRepository.findByEngraving1AndEngraving2(subClass.getEngraving1(), subClass.getEngraving2());
+        } else if (!subClass.getEngraving1().equals(EMPTY)
                 & subClass.getEngraving2().equals(EMPTY)
                 & !subClass.getCharacteristic1().equals(EMPTY)
                 & subClass.getCharacteristic2().equals(EMPTY)) {
-        }
-        else if (!subClass.getEngraving1().equals(EMPTY)
+            return subClassRepository.findByEngraving1AndCharacteristic1(subClass.getEngraving1(), subClass.getCharacteristic1());
+        } else if (!subClass.getEngraving1().equals(EMPTY)
                 & subClass.getEngraving2().equals(EMPTY)
                 & subClass.getCharacteristic1().equals(EMPTY)
                 & !subClass.getCharacteristic2().equals(EMPTY)) {
-        }
-        else if (subClass.getEngraving1().equals(EMPTY)
-                & !subClass.getEngraving2().equals(EMPTY)
-                & !subClass.getCharacteristic1().equals(EMPTY)
-                & subClass.getCharacteristic2().equals(EMPTY)) {
-        }
-        else if (subClass.getEngraving1().equals(EMPTY)
-                & !subClass.getEngraving2().equals(EMPTY)
-                & subClass.getCharacteristic1().equals(EMPTY)
-                & !subClass.getCharacteristic2().equals(EMPTY)) {
-        }
-        else if (subClass.getEngraving1().equals(EMPTY)
-                & subClass.getEngraving2().equals(EMPTY)
-                & !subClass.getCharacteristic1().equals(EMPTY)
-                & !subClass.getCharacteristic2().equals(EMPTY)) {
-        }
-        else if (!subClass.getEngraving1().equals(EMPTY)
+            return subClassRepository.findByEngraving1AndCharacteristic2(subClass.getEngraving1(), subClass.getCharacteristic2());
+        } else if (subClass.getEngraving1().equals(EMPTY)
                 & !subClass.getEngraving2().equals(EMPTY)
                 & !subClass.getCharacteristic1().equals(EMPTY)
                 & subClass.getCharacteristic2().equals(EMPTY)) {
-        }
-        else if (subClass.getEngraving1().equals(EMPTY)
-                & !subClass.getEngraving2().equals(EMPTY)
-                & !subClass.getCharacteristic1().equals(EMPTY)
-                & !subClass.getCharacteristic2().equals(EMPTY)) {
-        }
-        else if (!subClass.getEngraving1().equals(EMPTY)
-                & subClass.getEngraving2().equals(EMPTY)
-                & !subClass.getCharacteristic1().equals(EMPTY)
-                & !subClass.getCharacteristic2().equals(EMPTY)) {
-        }
-        else if (!subClass.getEngraving1().equals(EMPTY)
+            return subClassRepository.findByEngraving2AndCharacteristic1(subClass.getEngraving2(), subClass.getCharacteristic1());
+        } else if (subClass.getEngraving1().equals(EMPTY)
                 & !subClass.getEngraving2().equals(EMPTY)
                 & subClass.getCharacteristic1().equals(EMPTY)
                 & !subClass.getCharacteristic2().equals(EMPTY)) {
-        }
-        else if (!subClass.getEngraving1().equals(EMPTY)
-                & !subClass.getEngraving2().equals(EMPTY)
+            return subClassRepository.findByEngraving2AndCharacteristic2(subClass.getEngraving2(), subClass.getCharacteristic2());
+        } else if (subClass.getEngraving1().equals(EMPTY)
+                & subClass.getEngraving2().equals(EMPTY)
                 & !subClass.getCharacteristic1().equals(EMPTY)
                 & !subClass.getCharacteristic2().equals(EMPTY)) {
-        }
-        else if (subClass.getEngraving1().equals(EMPTY)
-                & subClass.getEngraving2().equals(EMPTY)
-                & subClass.getCharacteristic1().equals(EMPTY)
+            return subClassRepository.findByCharacteristic1AndCharacteristic2(subClass.getCharacteristic1(), subClass.getCharacteristic2());
+        } else if (!subClass.getEngraving1().equals(EMPTY)
+                & !subClass.getEngraving2().equals(EMPTY)
+                & !subClass.getCharacteristic1().equals(EMPTY)
                 & subClass.getCharacteristic2().equals(EMPTY)) {
+            return subClassRepository.findByEngraving1AndEngraving2AndCharacteristic1(subClass.getEngraving1(), subClass.getEngraving2(), subClass.getCharacteristic1());
+        } else if (subClass.getEngraving1().equals(EMPTY)
+                & !subClass.getEngraving2().equals(EMPTY)
+                & !subClass.getCharacteristic1().equals(EMPTY)
+                & !subClass.getCharacteristic2().equals(EMPTY)) {
+            return subClassRepository.findByEngraving2AndCharacteristic1AndCharacteristic2(subClass.getEngraving2(), subClass.getCharacteristic1(), subClass.getCharacteristic2());
+        } else if (!subClass.getEngraving1().equals(EMPTY)
+                & subClass.getEngraving2().equals(EMPTY)
+                & !subClass.getCharacteristic1().equals(EMPTY)
+                & !subClass.getCharacteristic2().equals(EMPTY)) {
+            return subClassRepository.findByEngraving1AndCharacteristic1AndCharacteristic2(subClass.getEngraving1(), subClass.getCharacteristic1(), subClass.getCharacteristic2());
+        } else if (!subClass.getEngraving1().equals(EMPTY)
+                & !subClass.getEngraving2().equals(EMPTY)
+                & subClass.getCharacteristic1().equals(EMPTY)
+                & !subClass.getCharacteristic2().equals(EMPTY)) {
+            return subClassRepository.findByEngraving1AndEngraving2AndCharacteristic2(subClass.getEngraving1(), subClass.getEngraving2(), subClass.getCharacteristic2());
+        } else if (!subClass.getEngraving1().equals(EMPTY)
+                & !subClass.getEngraving2().equals(EMPTY)
+                & !subClass.getCharacteristic1().equals(EMPTY)
+                & !subClass.getCharacteristic2().equals(EMPTY)) {
+            return subClassRepository.findByEngraving1AndEngraving2AndCharacteristic1AndCharacteristic2(subClass.getEngraving1(), subClass.getEngraving2(), subClass.getCharacteristic1(), subClass.getCharacteristic2());
         }
-
-        SubClass test = new SubClass();
-        test.setName("test");
-        test.setCharacteristic1("test");
-        List<SubClass> listTest = new ArrayList<>();
-        listTest.add(test);
-        return listTest;
+        return subClassRepository.findAll();
     }
 }
